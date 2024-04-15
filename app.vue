0cc8dc68d42d441efe03ba1b1ab86d7ddb5f3118
Kevin Burkett <stlbucket@gmail.com>
2024-04-14 17:34:34 -0700

    init commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <UButton 
+      :disabled="disableButton"
+      @click="onClicked"
+    >This is a button</UButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const disableButton = computed(() => Math.random() > .5)
+
+  const onClicked = () => {
+    alert('clicked')
+  }
+</script>
